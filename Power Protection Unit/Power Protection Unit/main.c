430ce257ce2173afeae5427555bbfbb4b477e98b
Nims98 <nirmalasabaragamuwa@gmail.com>
2022-11-13 18:34:12 +0530

    Code for power protection unit added

---
+++ Power Protection Unit/Power Protection Unit/main.c
@@ -0,0 +1,18 @@
+/*
+ * Power Protection Unit.c
+ *
+ * Created: 08/13/2022 4:30:58 PM
+ * Author : Nirmala
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
